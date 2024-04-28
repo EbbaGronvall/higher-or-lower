6869ff67623bfb5881f1bf4f16d27b68309fdb23
EbbaGronvall <ebbagronvall@hotmail.com>
2024-04-28 14:35:29 +0200

    Welcome message added

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
+import random
+def welcome_message():
+    """
+    Welcomes the user to the game and gives the instructions.
+    """
+    print("Welcome to Higher or Lower!\nIn this game you are guessing which random number the computer have.\n")
+    print("To start with you have to choose the difficulty.\nThere is easy(numbers 1-10), medium(numbers 1-15) and hard(numbers 1-20)\n")  
+    print("Then you will be asked to choose how many rounds you wanna go. 3, 5 or 10\n") 
+    print("Say you pick easy and 3 rounds. You will be asked to say a number between 1 and 10. If it is correct the computer will tell you so. If not the computer will tell you if the number is higher or lower. If you get it right this time you win the round, if not the computer wins the round.")
+    print("\nSo lets get started!")
+
+
+welcome_message()    

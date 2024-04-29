eae1f618bb9fe270b5003a3516b3c7a14dd6c4eb
EbbaGronvall <ebbagronvall@hotmail.com>
2024-04-29 19:23:03 +0200

    Added code so that run_game generates a new number each round

--- run.py
+++ run.py
@@ -3,19 +3,19 @@ from colorama import Fore, init
 from pyfiglet import Figlet
 f = Figlet(font='big')
 init(autoreset=True)
+
 def welcome_message():
     """
     Welcomes the user to the game and gives the instructions.
     """
-    
     print(f.renderText("Welcome to Higher or Lower!"))
     print("In this game you are guessing which random number the computer have.")
-    print("To start with you have to choose the difficulty.\nThere is easy(numbers 1-10), medium(numbers 1-15) and hard(numbers 1-20)\n")  
+    print("To start with you have to choose the difficulty.\n")
+    print("There is easy(numbers 1-10), medium(numbers 1-15) and hard(numbers 1-20)\n")  
     print("Then you will be asked to choose how many rounds you wanna go. 3, 5 or 10\n") 
     print("Say you pick easy and 3 rounds. You will be asked to say a number between 1 and 10. If it is correct the computer will tell you so. If not the computer will tell you if the number is higher or lower. You have 3 attempts to get it right otherwise the computer wins the round.")
     print("\nSo lets get started!")
 
- 
 def choose_difficulty():
     """
     The user chooses the difficulty
@@ -31,7 +31,6 @@ def choose_difficulty():
             return difficulty, difficulties[difficulty]
         else:
             print(Fore.RED + "Invalid difficulty! Please choose between easy, medium or hard!")
-    
 
 def generate_number(difficulty_upper_bound):
     """
@@ -47,9 +46,9 @@ def choose_rounds():
     while rounds not in ["3", "5", "10"]:
         print(Fore.RED + "Invalid number of rounds. Please choose 3, 5 or 10.")
         rounds = input("Choose the number of rounds (3, 5 or 10): ")
-    return int(rounds)            
-
-def run_game(difficulty_name, difficulty_upper_bound, rounds, secret_number):
+    return int(rounds) 
+           
+def run_game(difficulty_upper_bound, rounds, secret_number):
     """
     A function that runs the game once difficulty and number of rounds are chosen
     """
@@ -57,6 +56,7 @@ def run_game(difficulty_name, difficulty_upper_bound, rounds, secret_number):
     computer_score = 0 
     for round_num in range(1, rounds + 1):
         print(f"\nRound {round_num}:")
+        secret_number = generate_number(difficulty_upper_bound)
         print("The computer has picked a number.")
         guess = None
         attempts = 0
@@ -87,11 +87,8 @@ def run_game(difficulty_name, difficulty_upper_bound, rounds, secret_number):
         print(Fore.GREEN + f.renderText("YOU WON!"))  
         print(f"You won by {user_score} to {computer_score}!")                               
 
-
 welcome_message() 
 difficulty_name, difficulty_upper_bound = choose_difficulty()
 rounds = choose_rounds()   
 secret_number = generate_number(difficulty_upper_bound)
-run_game(difficulty_name, difficulty_upper_bound, rounds, secret_number)
-
-                              
+run_game(difficulty_upper_bound, rounds, secret_number)                           
